1f9d8dee6954f2b2062c9da09f3e05b24ccd5654
AndriuuU <66785221+AndriuuU@users.noreply.github.com>
2022-10-11 09:58:08 +0200

    Insertados datos en las tablas

--- tablas.sql
+++ tablas.sql
@@ -6,18 +6,25 @@ CREATE TABLE Alumnos (
     fecha_nac DATE,
     foto VARCHAR(100),
     telefono int(9),
-    pass VARCHAR(20),
+    pass VARCHAR(20) not null,
     CONSTRAINT alumnos_dni_pk PRIMARY KEY (dni)
 );
+
+INSERT INTO `alumnos` VALUES ('123456789A','Andres','Molina Gonzalez','andres@gmail.com','2001-08-09','/foto/AndresMolina',651321562,'molina');
+INSERT INTO `alumnos` VALUES ('987654321B','Nacho','Franco','nache@gmail.com','2000-01-02','/foto/NachoFranco',234567812,'franco');
+INSERT INTO `alumnos` VALUES ('123456324C','Juan Jeusu','Fernandez','juanjeusus@gmail.com','2000-04-03','/foto/JuanJesus',98765434,'fernandez');
 
 CREATE TABLE Profesor(
     dni VARCHAR(10),
     nombre VARCHAR(100),
     apellidos VARCHAR(200),
     email VARCHAR(100),
-    pass VARCHAR(20),
+    pass VARCHAR(20) not null,
     CONSTRAINT profesor_dni_pk PRIMARY KEY (dni)
 );
+
+INSERT INTO `profesor` VALUES ('74544252J','Raul','Reyes','raulreyes@gmail.com','reyes');
+INSERT INTO `profesor` VALUES ('9723412523H','David','Mateo','davidmateo@gmail.com','mateo');
 
 CREATE TABLE Asignatura(
     codAsig VARCHAR(5),
@@ -27,6 +34,9 @@ CREATE TABLE Asignatura(
     CONSTRAINT Asignatura_nomb_pk PRIMARY KEY (codAsig),
     CONSTRAINT asignatura_dniProfe_fk FOREIGN KEY (dniProfesor) REFERENCES Profesor(dni)
 );
+
+INSERT INTO `asignatura` VALUES ('Prog-123','Programacion JAVA',350,'74544252J');
+INSERT INTO `asignatura` VALUES ('DI-321','Desarrollo de interfaces',300,'9723412523H');
 
 CREATE TABLE RA(
     id VARCHAR(5),
@@ -38,6 +48,12 @@ CREATE TABLE RA(
     CONSTRAINT ra_codAsig_fk FOREIGN KEY (codAsig) REFERENCES Asignatura(codAsig)
 );
 
+INSERT INTO `ra` VALUES ('Ra1-prog','Bucles','Aprender a hacer figuras con bucles',50,'Prog-123');
+INSERT INTO `ra` VALUES ('Ra2-prog','Arrays','Desarollar una biblioteca con arrays',50,'Prog-123');
+
+INSERT INTO `ra` VALUES ('Ra1-di','Laravel','Aprender laravel',25,'DI-321');
+INSERT INTO `ra` VALUES ('Ra2-di','Web','Desarrollar una pagina web',75,'DI-321');
+
 CREATE TABLE Matricula (
     dniAlumno VARCHAR(10),
     codAsig VARCHAR(5),
@@ -46,6 +62,14 @@ CREATE TABLE Matricula (
     CONSTRAINT matricula_dni_fk foreign key (dniAlumno) references Alumnos(dni)
 );
 
+INSERT INTO `matricula` VALUES ('123456789A','Prog-123');
+INSERT INTO `matricula` VALUES ('123456789A','DI-321');
+
+INSERT INTO `matricula` VALUES ('987654321B','Prog-123');
+
+INSERT INTO `matricula` VALUES ('123456324C','Prog-123');
+INSERT INTO `matricula` VALUES ('123456324C','DI-321');
+
 CREATE TABLE Califica (
     dniAlumno VARCHAR(10),
     idRa VARCHAR(5),
@@ -53,5 +77,16 @@ CREATE TABLE Califica (
     CONSTRAINT califica_dni_codRa_pk PRIMARY KEY (dniAlumno,idRa),
     CONSTRAINT califica_codAsi_fk foreign key (idRa) references RA(id),
     CONSTRAINT califica_dni_fk foreign key (dniAlumno) references Alumnos(dni)
-
 );
+
+INSERT INTO `califica` VALUES ('123456789A','Ra1-prog',8);
+INSERT INTO `califica` VALUES ('123456789A','Ra2-prog',7);
+INSERT INTO `califica` VALUES ('123456789A','Ra1-di',8);
+INSERT INTO `califica` VALUES ('123456789A','Ra2-di',7);
+
+INSERT INTO `califica` VALUES ('987654321B','Ra1-prog',9);
+
+INSERT INTO `califica` VALUES ('123456324C','Ra1-prog',6);
+INSERT INTO `califica` VALUES ('123456324C','Ra2-prog',5);
+INSERT INTO `califica` VALUES ('123456324C','Ra1-di',3);
+INSERT INTO `califica` VALUES ('123456324C','Ra2-di',4);

62c68c29627b8b67e7abb5c4f59d9bd0ae8878ff
Juan Jesús Fernández Rodríguez <alba-juanjesus@hotmail.es>
2022-10-17 10:39:42 +0200

    Continue of AdminWindow
    Modificación del apartado estético de la ventana que se muestra al ser administrador. Modificación de la configuración predeterminada de los botones para añadir los iconos adjuntando la ruta en el propio método. Adjunción de los iconos a la carpeta de archivos.

--- TrabajoRa2/windows/WindowPreset.java
+++ TrabajoRa2/windows/WindowPreset.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Image;
 import java.awt.Toolkit;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -11,18 +13,20 @@ public class WindowPreset {
 
 	public static void preset(JFrame jf) {
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setBackground(Color.GRAY);
+		jf.getContentPane().setBackground(Color.LIGHT_GRAY);
 		jf.setLocationRelativeTo(null);
 		jf.setResizable(false);
 		Image icon1 = Toolkit.getDefaultToolkit().getImage("files/icon.png");
 		jf.setIconImage(icon1);
 	}
 
-	public static void buttonPreset(JButton B, String T) {
+	public static void buttonPreset(JButton B, String T, String U) {
 
+		Icon ic = new ImageIcon(U);
 		B.setBackground(Color.LIGHT_GRAY);
 		B.setBorderPainted(false);
 		B.setToolTipText(T);
+		B.setIcon(ic);
 
 	}
 
